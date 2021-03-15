39ee23fca8aab943a4c98c878a57d73b6b9a9bae
Giulio <>
2021-03-15 08:24:49 +0100

    Remove whitespace

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     $session_id = WorkFlowy::login($_ENV['USERNAME'], $_ENV['PASSWORD']);
     $list = (new WorkFlowyList($session_id))->getList();
     $cal = "BEGIN:VCALENDAR
-        VERSION:2.0
-        PRODID:wfcal
-        ";
+VERSION:2.0
+PRODID:wfcal
+";
 
     function parse($node) {
         if (preg_match('/(.*?)<time startYear="(\d+)" startMonth="(\d+)" startDay="(\d+)">.*?<\/time>(.*?)/', $node->getName(), $m)) {
@@ -21,14 +21,14 @@
                 .str_pad($m[4],2,'0',STR_PAD_LEFT);
             $timestamp = (new DateTime("$m[2]-$m[3]-$m[4]"))->getTimestamp();
             $GLOBALS['cal'] .= "BEGIN:VEVENT
-                UID:{$node->getID()}
-                DTSTAMP:$timestamp
-                DTSTART;VALUE=DATE:$date
-                DTEND;VALUE=DATE:$date
-                SUMMARY:{$m[1]}{$m[5]}
-                DESCRIPTION:{$node->getDescription()}
-                END:VEVENT
-                ";
+UID:{$node->getID()}
+DTSTAMP:$timestamp
+DTSTART;VALUE=DATE:$date
+DTEND;VALUE=DATE:$date
+SUMMARY:{$m[1]}{$m[5]}
+DESCRIPTION:{$node->getDescription()}
+END:VEVENT
+";
         }
         foreach ($node->getSublists() as $subnode)
             parse($subnode);
